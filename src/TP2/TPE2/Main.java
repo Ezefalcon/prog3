f631d1e2e5fe6f5f0828d91ad7370c352f6143e5
Ezequiel Falcon <exzequielfalcon@gmail.com>
2020-05-02 11:49:07 -0300

    Changed delete sout

--- src/TP2/TPE2/Main.java
+++ src/TP2/TPE2/Main.java
@@ -26,7 +26,7 @@ public class Main {
         System.out.println("Frontier -> " + tree.getFrontier());
         System.out.println("Elements at level -> " + tree.getElemAtLevel(3));
         System.out.println("Longest branch -> " + tree.getLongestBranch());
-        System.out.println("Delete 22 and printInOrder -> " + tree.delete(31) + " -> ");
+        System.out.println("Delete 31 and printInOrder -> " + tree.delete(31) + " -> ");
         tree.printInOrder();
 
         Tree treeRandomized = new Tree();
